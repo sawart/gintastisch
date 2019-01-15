65232c24d8b9092fcef3142e934342f1a0f6dc6f
Aufbau Header <rene@still-awake.net>
2019-01-15 17:18:32 +0100

    starting content choreography / introWrapper done / start logo grid

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 	$(".hamburger").on("click", function(){
 		$(this).toggleClass("is-active");
+		
+		if($(".hamburger").hasClass("is-active")) {
+			$(".cl-effect-5").css("display", "flex");
+		} else {
+			$(".cl-effect-5").css("display", "none");
+		}
 	});
 });
+
+/*
+
+$(window).resize(function(){
+
+       if ($(window).width() <= 992) {  
+
+   
+
+       }     
+
+});
+
+*/
